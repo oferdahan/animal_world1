ca5ac44d7f5c7135b0b7ffba72179b2fd1a1ccc7
oferd845 <oferd845@gmail.com>
2022-12-24 10:23:15 +0200

    i_all

--- animal_world/src/App.tsx
+++ animal_world/src/App.tsx
@@ -1,13 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <div className="App container  ">
       app work
+
+      <div>
+
+      
+
+      <Header />
+      </div>
+      <Footer />
     </div>
+    </>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ animal_world/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (  
+        <>
+        Footer work
+        </>
+    );
+}
+
+export default Footer;
